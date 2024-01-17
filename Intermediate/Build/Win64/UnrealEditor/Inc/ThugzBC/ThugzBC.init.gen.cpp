e800f746ef651dac30aa00febc2a16101adb4e2e
Lockeff <120092937+Lockeff@users.noreply.github.com>
2024-01-17 14:57:24 +0100

    Initialisation

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ThugzBC/ThugzBC.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeThugzBC_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ThugzBC;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ThugzBC()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ThugzBC.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ThugzBC",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x464D6CF8,
+				0x9D14740F,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ThugzBC.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ThugzBC.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ThugzBC(Z_Construct_UPackage__Script_ThugzBC, TEXT("/Script/ThugzBC"), Z_Registration_Info_UPackage__Script_ThugzBC, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x464D6CF8, 0x9D14740F));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
